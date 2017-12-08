3b44efe6125a255d215153e885d6cf7a7b4fe89b
K-U- <ken77.ilink2@mac.com>
2017-12-08 17:29:37 +0900

    素材追加

--- Assets/Editor/CustomSceneOpener.cs
+++ Assets/Editor/CustomSceneOpener.cs
@@ -9,8 +9,7 @@ public class CustomSceneOpener : EditorWindow {
 
     [MenuItem("Tools/Util/Custom Scene Opener %#t")]
     public static void OpenDialog(){
-
-        var window = EditorWindow.GetWindow<CustomSceneOpener> ("SceneOpener");
+        var window = EditorWindow.GetWindow<CustomSceneOpener> ("CustomSceneOpener");
         window.Focus ();
         window.InitializeScenes ();
     }
@@ -22,35 +21,33 @@ public class CustomSceneOpener : EditorWindow {
             Where ((arg) => arg.EndsWith (".unity")).ToArray ();
         sceneName = "";
     }
-    public void OnGUI (){
-        GUILayout.BeginVertical ();
-        GUI.SetNextControlName ("Input");
-        sceneName = GUILayout.TextField (sceneName);
-        GUI.FocusControl ("Input");
-        var temp = allScenes;
-        if (!string.IsNullOrEmpty (sceneName)){ 
-            temp = allScenes.Where ((arg) => Path.GetFileName (arg).Replace (".unity", "").ToLower().Contains (sceneName.ToLower())).ToArray();
-        }
 
-        foreach (var scene in temp){
-            GUILayout.BeginHorizontal ();
-            GUILayout.Label (Path.GetFileName (scene));
-            GUILayout.FlexibleSpace ();
-            if (GUILayout.Button ("Open")){
+    public void OnGUI (){
+        GUILayout.BeginVertical ();{
+            GUI.SetNextControlName ("Input");
+            sceneName = GUILayout.TextField (sceneName);
+            GUI.FocusControl ("Input");
+            var temp = allScenes;
+            if (!string.IsNullOrEmpty (sceneName)){ 
+                temp = allScenes.Where ((arg) => Path.GetFileName (arg).Replace (".unity", "").ToLower().Contains (sceneName.ToLower())).ToArray();
+            }
+            foreach (var scene in temp){
+                GUILayout.BeginHorizontal ();{
+                    GUILayout.Label (Path.GetFileName (scene));
+                    GUILayout.FlexibleSpace ();
+                    if (GUILayout.Button ("Open")){
+                        OpenScene (scene);
+                    }
+                }GUILayout.EndHorizontal ();
+            }
+            if (Event.current.keyCode == KeyCode.Return && temp.Length > 0){
+                var scene = temp.First ();
                 OpenScene (scene);
             }
-            GUILayout.EndHorizontal ();
-        }
 
-        if (Event.current.keyCode == KeyCode.Return && temp.Length > 0){
-            var scene = temp.First ();
-            OpenScene (scene);
-        }
-
-        GUILayout.EndVertical ();
+        }GUILayout.EndVertical ();
     }
     private void OpenScene(string scene){
-
         if (UnityEditor.SceneManagement.EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo ()){
             UnityEditor.SceneManagement.EditorSceneManager.OpenScene (scene, UnityEditor.SceneManagement.OpenSceneMode.Single);
             this.Close ();
